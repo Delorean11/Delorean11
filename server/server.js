da6b65da15399aedeebcb807f01d2fbfe066f0dd
Sean Reimer <sreimer15@gmail.com>
2016-01-08 13:30:39 -0600

    Adds cookieParsers and BodyParser MiddleWare

--- server/server.js
+++ server/server.js
@@ -7,16 +7,24 @@ var User = require('./models/userModel');
 var CongressPerson = require('./models/congressPersonModel');
 var seedConstructors = require('./models/seedConstructors');
 
+// Dependencies needed for Authentication
+var passport = require('passport');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+//
+
+
 var app = express();
 var port = process.env.PORT || 4556;
 app.use(express.static(__dirname + '/../client'));
 app.listen(port);
 app.use(morgan('dev'));
-
-app.use('/api', routes);
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 
 console.log('Server now listening on port ' + port);
-
+app.use('/api', routes);
 //Connect to db
 mongoose.connect('mongodb://localhost/congressionalStalker'); //Test local db
 //mongoose.connect("mongodb://delorean11:delorean11@ds039175.mongolab.com:39175/congressional-stalker");
