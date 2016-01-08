3ade3e9cd70c11433eb54e497b4f952ed9a90391
Santosh Gautam <santoshgautam7@gmail.com>
2016-01-07 17:52:21 -0800

    Connected to API closed #20

--- client/components/search/searchCtrl.js
+++ client/components/search/searchCtrl.js
@@ -1,4 +1,18 @@
 angular.module('Search', [])
-.controller('SearchController',['$scope', function($scope){
+.controller('SearchController',['$scope', '$http', function($scope, $http){
+  var congressNumber = '113';
+  var house = 'house';
+  var api_key = 'dab50f4c71783810c9a7c1f132ef3136:5:73959417';
+  $scope.getAllMembers = function() {
+    $http({
+      method: 'GET',
+      url: 'http://api.nytimes.com/svc/politics/v3/us/legislative/congress/' + congressNumber + '/' + house + '/members.json?api-key=' + api_key
+    })
+    .success(function(data) {
+      $scope.memberData = data.results[0].members;
+      console.log(data.results[0].members);
+    })
+  };
 
+  $scope.getAllMembers();
 }]);
