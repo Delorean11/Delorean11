49c198c35ab8cd37a8278644525f0b39b36db333
Delphine Foo-Matkin <delphinef@gmail.com>
2016-01-10 10:59:50 -0800

    Implemented logout.

--- client/auth/logout.js
+++ client/auth/logout.js
@@ -1,12 +1,15 @@
 angular.module('Logout', [])
-.controller('LogoutController', ['$http', function($http){
+.controller('LogoutController', ['$http','$state', function($http, $state){
   var newHttpRequest = function(){
     $http({
       method: 'GET',
       url: '/api/logout'
     }).success(function(data){
-      console.log(data);
+      localStorage.removeItem('loginKey');
+      setTimeout(function() {
+        $state.go('main');
+      }, 3000);
     });
-  }
+  };
   newHttpRequest();
-}])
+}]);
