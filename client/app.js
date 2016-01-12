3616473f0ec4668df095fbf32fd4ea9f4ccb95fc
Delphine Foo-Matkin <delphinef@gmail.com>
2016-01-12 08:30:34 -0800

    Added comments. Added toLowerCase call to search view.

--- client/app.js
+++ client/app.js
@@ -18,15 +18,19 @@ angular.module('CongressionalStalker', [
   };
   $rootScope.searchCacheCheck = function(){
     return JSON.parse(localStorage.getItem('searchCache')).length > 0;
-  }
+  };
+
+  /* The nameCase function is used to capitalize names when displayed.
+  The database stores congress member's names in lowercase. */
   $rootScope.nameCase = function(name){
+    name.toLowerCase();
     var split = name.split(' ');
     for(var i = 0; i < 2; i++){
       var firstLetter = String.fromCharCode(split[i].charCodeAt(0) - 32);
       split[i] = firstLetter + split[i].slice(1);
     }
     return split.join(' ');
-  }
+  };
 })
 .config(['$stateProvider', '$urlRouterProvider', '$httpProvider',
   function($stateProvider, $urlRouterProvider, $httpProvider) {
@@ -72,21 +76,6 @@ angular.module('CongressionalStalker', [
       templateUrl: 'components/results/byStateResults.html',
       controller: 'ByStateResultsController'
     });
-}])
-.factory('SearchConnector', ['$http', function($http){
-  // Factory exists to pass objects from one controller to the next
-  // In order to access we need to call resultObject function passing in your queryResult as a parameter to your callback function
-  var resultObject = function (queryResult, cb){
-    cb(queryResult);
-  };
-    /* example implementation:
-      SearchConnector.resultObject(testObject, function(passedObject){
-           console.log(passedObject);
-        });
-  */
-  return {
-    resultObject: resultObject
-  };
 }])
 .run(function($rootScope){
   var updateSearchCache = function(){
