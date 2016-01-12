6e95767b58628c597d1140dff1ef25bb265a3aa1
Santosh Gautam <santoshgautam7@gmail.com>
2016-01-12 01:44:43 -0800

    Added teamMembers. Made some minor tweaks in results page. Added spinner to searchByState page.

--- client/components/filters/filters.js
+++ client/components/filters/filters.js
@@ -22,4 +22,13 @@ angular.module('DlFilters', [])
                 return ch.toUpperCase();
             });
         };
-  });
+  })
+  .filter('startFrom', function() {
+    return function(input, start) {
+      if(input) {
+          start = +start; //parse to int
+          return input.slice(start);
+      }
+      return [];
+    }
+});
--- client/components/search/searchCtrl.js
+++ client/components/search/searchCtrl.js
@@ -1,4 +1,29 @@
 angular.module('Search', [])
 .controller('SearchController',['$scope', '$rootScope', '$state', 'SendRequest', function($scope, $rootScope, $state, SendRequest){
+  $scope.teamMembers = 
+  [
+    {
+      name: 'Bryan Bierce',
+      role: 'Software Engineer', 
+      image: '/images/bryan.jpg'
+    },
 
+    {
+      name: 'Delphine Foo-Matkin',
+      role: 'Software Engineer', 
+      image: '/images/delphine.png'
+    },
+
+    {
+      name: 'Santosh Gautam',
+      role: 'Scrum Master', 
+      image: '/images/santosh.png'
+    },
+
+    {
+      name: 'Sean Reimer',
+      role: 'Product Owner', 
+      image: '/images/sean.jpg'
+    }
+  ]
 }]);
